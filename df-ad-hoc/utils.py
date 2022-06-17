db61f60132c7b9a1ca684c0b23283f17c459014a
Andrey Belchenko <abelchenko@conteq.ru>
2022-06-17 06:14:46 +0300

    1

--- df-ad-hoc/utils.py
+++ df-ad-hoc/utils.py
@@ -10,10 +10,10 @@ def execSparql(endpoint, query):
     return sparql.queryAndConvert()
 
 
-def dataFrameFromSparqlResult(result, removePref)->pd.DataFrame:
+def dataFrameFromSparqlResult(result, removePref) -> pd.DataFrame:
     tbl = []
     titles = []
-    
+
     for r in result["results"]["bindings"]:
         row = []
         tbl.append(row)
@@ -21,14 +21,24 @@ def dataFrameFromSparqlResult(result, removePref)->pd.DataFrame:
         for f in r:
             val = r[f]["value"]
             if removePref:
-                #частный случай, не универсально
-                val = val.split("#")[-1].split("/")[-1].split(".")[-1]
+                # частный случай, не универсально
+                val = val.split("#")[-1].split("/")[-1]#.split(".")[-1]
             row.append(val)
             if i > len(titles):
                 titles.append(f)
             i += 1
     df = pd.DataFrame(tbl, columns=titles)
     return df
+
+def dataFrameToObjects(df: pd.DataFrame) -> list:
+    list = []
+    for ind in df.index:
+        obj = {}
+        for column in df:
+            obj[column]=df[column][ind]
+            # setattr(obj, column, df[column][ind])
+        list.append(obj)
+    return list
 
 
 def sparqlDF(endpoint, query, removePref=False):
